b3973527408eea90a72f82cde4c78280c8a178b3
mandy <mandeepnain84@gmail.com>
2023-09-15 13:55:13 +0530

    basic sigin page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import { TextField } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <TextField id='standard-basic' label = 'Email' variant='standard' type = 'email' />
+      </div>
+      <div>
+      <TextField id='standard-basic' label = 'Password' variant='standard' type='password' />
+      </div>
+      <div>
+      <Button
+     color='secondary'
+     variant='outlined'
+     >Submit</Button>
+      </div>
+     
     </div>
   );
 }
